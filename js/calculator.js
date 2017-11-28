a2269569346f21b9cb24a9a3445fc920a45cbb06
Unknown <baptiste.sanchez72@gmail.com>
2017-11-28 15:31:54 +0100

    Fonction reset lors de l'appuis d'un nouveau nombre.

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,13 @@
 // add your code here to make the calculator work
 var nombre1;
 var nombre2;
-
+var reset = false;
 
 $('.btn').click(function(){
+	if (reset == true){
+		$('#result').html('');
+		reset = false;
+	}
 	if ($(this).html() === 'c') {
 		$('#result').html('');
 	}else if ($(this).html() === '=') {
@@ -13,6 +17,7 @@ $('.btn').click(function(){
 			nombre2 = $('#result').html().substr($('#result').html().indexOf('+')+1, $('#result').html().length);
 			var resultat = parseInt(nombre1) + parseInt(nombre2);
 			$('#result').html(resultat);
+			reset = true;
 		}
 		if ($('#result').html().indexOf('-') !== -1) {
 			nombre1 = $('#result').html().substr(0, $('#result').html().indexOf('-'));
