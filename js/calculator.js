9a39d1a06d83bdfd169ea7b91ebc1c057f3253c6
Unknown <baptiste.sanchez72@gmail.com>
2017-11-28 14:45:39 +0100

    Condition pour chaque calculs

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,47 @@
 // add your code here to make the calculator work
+var nombre1;
+var nombre2;
 
 
 $('.btn').click(function(){
-	$('#result').append($(this).html());
-	$(console.log('test'));
+	
+	if ($(this).html() === 'c') {
+		$('#result').html('');
+	}else if ($(this).html() === '=') {
+		if ($('#result').html().indexOf('+') !== -1) {
+			console.log($('#result').html().indexOf('+'));
+			nombre1 = $('#result').html().substr(0, $('#result').html().indexOf('+'));
+			nombre2 = $('#result').html().substr($('#result').html().indexOf('+')+1, $('#result').html().length);
+			console.log(nombre1);
+			console.log(nombre2);
+			parseInt(nombre1);
+			parseInt(nombre2);
+			nombre1 += nombre2;
+			console.log(nombre1);
+		}
+		if ($('#result').html().indexOf('-') !== -1) {
+
+		}
+		if ($('#result').html().indexOf('/') !== -1) {
+
+		}
+		if ($('#result').html().indexOf('*') !== -1) {
+
+		}
+	} else {
+		$('#result').append($(this).html());
+	}
 });
+
+// function addition(){
+
+
+	/*$('#result').append($(this).html() + $(this).html());*/
+// }
+
+
+
+
+
+
 
