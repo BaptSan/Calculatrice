5d4fbe3ec3e0358478d67288064fa58b9a1328d2
Unknown <baptiste.sanchez72@gmail.com>
2017-11-28 15:13:04 +0100

    calculs oppérationnel pour chaque calculs.
    (bug lors de succession de calculs, (ex: 2 + 2 + 2 = 4)

--- js/calculator.js
+++ js/calculator.js
@@ -4,7 +4,6 @@ var nombre2;
 
 
 $('.btn').click(function(){
-	
 	if ($(this).html() === 'c') {
 		$('#result').html('');
 	}else if ($(this).html() === '=') {
@@ -12,21 +11,26 @@ $('.btn').click(function(){
 			console.log($('#result').html().indexOf('+'));
 			nombre1 = $('#result').html().substr(0, $('#result').html().indexOf('+'));
 			nombre2 = $('#result').html().substr($('#result').html().indexOf('+')+1, $('#result').html().length);
-			console.log(nombre1);
-			console.log(nombre2);
-			parseInt(nombre1);
-			parseInt(nombre2);
-			nombre1 += nombre2;
-			console.log(nombre1);
+			var resultat = parseInt(nombre1) + parseInt(nombre2);
+			$('#result').html(resultat);
 		}
 		if ($('#result').html().indexOf('-') !== -1) {
-
+			nombre1 = $('#result').html().substr(0, $('#result').html().indexOf('-'));
+			nombre2 = $('#result').html().substr($('#result').html().indexOf('-')+1, $('#result').html().length);
+			var resultat = parseInt(nombre1) - parseInt(nombre2);
+			$('#result').html(resultat);
 		}
 		if ($('#result').html().indexOf('/') !== -1) {
-
+			nombre1 = $('#result').html().substr(0, $('#result').html().indexOf('/'));
+			nombre2 = $('#result').html().substr($('#result').html().indexOf('/')+1, $('#result').html().length);
+			var resultat = parseInt(nombre1) / parseInt(nombre2);
+			$('#result').html(resultat);
 		}
 		if ($('#result').html().indexOf('*') !== -1) {
-
+			nombre1 = $('#result').html().substr(0, $('#result').html().indexOf('*'));
+			nombre2 = $('#result').html().substr($('#result').html().indexOf('*')+1, $('#result').html().length);
+			var resultat = parseInt(nombre1) * parseInt(nombre2);
+			$('#result').html(resultat);
 		}
 	} else {
 		$('#result').append($(this).html());
